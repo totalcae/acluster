b1a3c9775d8698b84923a93031fd38d14f39e9f0
TotalCAE <rod@totalcae.com>
2018-11-05 10:43:16 -0500

    Update azuredeploy.sh

--- azuredeploy.sh
+++ azuredeploy.sh
@@ -1,5 +1,36 @@
-#!/bin/bash
 
-printenv > /tmp/env
+# TotalCAE CONFIDENTIAL
+#
+#
+# 2018 TotalCAE
+# All Rights Reserved.
+#
+# NOTICE: All information contained herein is, and remains
+# the property of TotalCAE
+#
+# The intellectual and technical concepts contained
+# herein are proprietary to TotalCAE
+# and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from TotalCAE
+#
+
+export ADMIN_USER=$1
+export ADMIN_PASSWORD=$2
+export LICENSE_KEY=$3
+export APPLICATION=$4
+export INSTANCE_COUNT=$5
+
+printenv > /tmp/ENVIRONMENT
+
+mkdir -p /srv/ 
+cd /srv
+/bin/wget https://totalcaeapplications.blob.core.windows.net/applications/ansible.tar.gpg
+echo ${LICENSE_KEY} |  gpg --batch --output ansible.tar --passphrase-fd O  --decrypt ansible.tar.gpg
+tar xfv ansible.tar
+cd totalcaeansible/azure
+yum -y install ansible
+./make-head.sh > /var/log/totalcaedeploy.log 2>&1
 exit 0
-
